06a0fed9afe2e70dbd9f21634919918578810681
Jada Joseph <ja857112@ucf.edu>
2020-01-15 10:19:55 -0500

    in class exercise

--- ice/w2a/node_index.js
+++ ice/w2a/node_index.js
@@ -1,11 +1,21 @@
+console.log("Hello World");
+var x = 1;
+var y = [1];
 function scopeDemo() {
     console.log(x)
     let a = 3, b = 4, c = 5;
     console.log(a);
     console.log(b);
     console.log(c);
+    console.log(y1);
     var x = 2;
     if(true) {
         var x = 0;
     }
+    return a;
+
 }
+console.log(scopeDemo(y)+10);
+console.log(y);
+console.log(0);
+
