0e444db9a5e629b6ab748a8589d3922f1e155b1f
JadaJoseph43 <42987213+JadaJoseph43@users.noreply.github.com>
2020-02-01 15:53:40 -0500

    Add files via upload

---
+++ labs/1/Adder.js
@@ -0,0 +1,15 @@
+class Adder {
+    props;
+    constructor(props){
+this.props = props;
+    }
+    sum(){
+return this.props.a + this.props.b;
+    }
+    render(){
+        
+        return `<p>The sum of ${this.props.a} and ${this.props.b} is 15</p>`;
+    }
+}
+
+module.exports = Adder;
