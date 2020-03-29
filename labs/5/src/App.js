a27c3d53d221d5e838f10d0dfe2696b8b1c02d3e
JadaJoseph43 <42987213+JadaJoseph43@users.noreply.github.com>
2020-03-29 17:37:42 -0400

    lab5

---
+++ labs/5/src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './App.css';
+import Card from './components/Card'
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {content:"", list: []}
+  }
+  
+  async getServerData() {
+    const response = await fetch("https://api.weather.gov/gridpoints/MLB/25,69/forecast")
+    console.log("Received response from server!")
+    console.log(response)
+    let obj = await response.json()
+    console.log("Processed response as JSON: ", obj)
+    this.setState({list:obj})
+  }
+
+  componentDidMount() {
+    console.log("Component did mount!")
+    this.getServerData()
+  }
+
+  render() {
+    return (
+      <div>
+        <p>{this.state.content}</p>
+        {this.state.list.map((listObject, index) =>
+        <Card key={index} title={listObject.name}>
+          {listObject.temperature}{listObject.temperatureUnit}
+          {listObject.detailedForecast}
+          </Card>
+        )}
+      </div>
+    )
+  }
+}
+
+export default App;
