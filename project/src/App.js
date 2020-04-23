cdce435098d5166dda97ac0487d29f89ec010f4c
JadaJoseph43 <42987213+JadaJoseph43@users.noreply.github.com>
2020-04-23 18:10:51 -0400

    complete

--- project/src/App.js
+++ project/src/App.js
@@ -1,18 +1,13 @@
-import React from 'react';
-import Add from "./components/newContact/index.js";
-import Contacts from "./components/contacts/index.js";
-import Remove from "./components/remove/index.js";
-import Profile from "./components/profile/index.js";
-import './App.css';
-
-function App() {
-  return (
-    <div className="Contacts">
-    <Contacts/>
-    <Add/>
   
-    </div>
-  );
+import React from 'react';
+import ContactApp from './components';
+
+class App extends React.Component {
+  render() {
+    return (
+        <ContactApp />
+    );
+  }
 }
 
 export default App;
